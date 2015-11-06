3da7def47290caf8e57a4f9b29bdc6280862b5eb
megatricycle <megatricycle@gmail.com>
2015-11-06 09:54:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#uLOLCODE Interpreter 
---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  var remote = require('remote');
+  var Menu = remote.require('menu');
+  var MenuItem = remote.require('menu-item');
+  var ipc = require('ipc');
+
+  // render menu
+  var template = [{
+    label: 'File',
+    submenu: [
+      {
+        label: 'Open file',
+        accelerator: 'CmdOrCtrl+O',
+        click: function(){
+          ipc.send('open-file');
+        }
+    }]
+  }];
+
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // http://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+  $(document).delegate('textarea', 'keydown', function(e) {
+    var keyCode = e.keyCode || e.which;
+
+    if (keyCode == 9) {
+      e.preventDefault();
+      var start = $(this).get(0).selectionStart;
+      var end = $(this).get(0).selectionEnd;
+
+      // set textarea value to: text before caret + tab + text after caret
+      $(this).val($(this).val().substring(0, start) + "\t" + $(this).val().substring(end));
+
+      // put caret at right position again
+      $(this).get(0).selectionStart =
+      $(this).get(0).selectionEnd = start + 1;
+    }
+  });
+});
