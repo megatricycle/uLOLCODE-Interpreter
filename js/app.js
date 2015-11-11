9d5d04dc7a4a5a1e909b4ac8f258c1149c121c8d
megatricycle <megatricycle@gmail.com>
2015-11-11 09:47:18 +0800

    fixed string implementation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// @TODO:
+// Negative NUMBR/NUMBAR
+// Initializing with variable
+
+// define regex here
+var regex = {
+  'NOOB': /^NOOB$/,
+  'TROOF': /^(WIN|FAIL)$/,
+  'NUMBR': /^-?\d+$/,
+  'NUMBAR': /^-?\d+\.\d+$/,
+  'YARN': /^".*"$/
+};
+
 $(document).ready(function(){
   var remote = require('remote');
   var Menu = remote.require('menu');
@@ -59,6 +72,9 @@ angular.module('app', []).controller('AppController', function($scope){
     // disallow execute on input mode
     if($scope.state == "input") return;
 
+    // print to console
+    printToConsole('Executing code.');
+
     // clear values
     $scope.lexemes = [];
     $scope.symbolTable = [];
@@ -77,28 +93,100 @@ angular.module('app', []).controller('AppController', function($scope){
       // remove excess lines
       lines[i] = lines[i].trim();
 
-      // test
+      // check keyword
       if(/^\s*HAI\s*$/.test(lines[i])){
-        addLexeme('HAI', 'green-text', 'Code delimeter');
+        addLexeme('HAI', 'green-text', 'Code Delimeter');
       }
       else if(/^\s*KTHXBYE\s*$/.test(lines[i])){
-        addLexeme('KTHXBYE', 'green-text', 'Code delimeter');
+        addLexeme('KTHXBYE', 'green-text', 'Code Delimeter');
       }
       else if(/\s*I HAS A\s+/.test(lines[i])){
-        addLexeme('I HAS A', 'green-text', 'Variable declaration');
+        addLexeme('I HAS A', 'green-text', 'Variable Declaration');
 
+        // remove whitespaces
         identifier = lines[i].substr(8).trim();
 
-        // add variable
-        addLexeme(identifier, 'white-text', 'Variable identifier');
+        // check if it has ITZ
+        if(/ ITZ /.test(identifier)){
+          // get index of ITZ
+          var index = identifier.indexOf('ITZ');
+
+          // get value
+          var value = identifier.substring(index + 3).trim();
+
+          // get identifier
+          identifier = identifier.substring(0, index).trim();
+
+          // identify type of value
+          var type;
+
+          if(regex.NOOB.test(value)){
+            // NOOB
+            type = 'NOOB';
+          }
+          else if(regex.TROOF.test(value)){
+            // TROOF
+            type = 'TROOF';
+          }
+          else if(regex.NUMBR.test(value)){
+            // NUMBR
+            type = 'NUMBR';
+          }
+          else if(regex.NUMBAR.test(value)){
+            // NUMBAR
+            type = 'NUMBAR';
+          }
+          else if(regex.YARN.test(value)){
+            // YARN
+            type = 'YARN';
+          }
+          else{
+            // variable
+          }
+
+          // add lexemes
+          // identifier
+          addLexeme(identifier, 'white-text', 'Variable Identifier');
+
+          // add ITZ
+          addLexeme('ITZ', 'green-text', 'Variable Assignment');
+
+          // add value
+          switch(type){
+            case 'NOOB':
+              addLexeme(value, 'yellow-text', 'Null Literal');
+              break;
+            case 'TROOF':
+              addLexeme(value, 'red-text', 'Boolean Literal');
+              break;
+            case 'NUMBR':
+              addLexeme(value, 'white-text', 'Integer Literal');
+              break;
+            case 'NUMBAR':
+              addLexeme(value, 'white-text', 'Float Literal');
+              break;
+            case 'YARN':
+              // omit string delimeters
+              value = value.substring(1, value.length - 1);
+              addLexeme('"', 'blue-text', 'String Delimeter');
+              addLexeme(value, 'blue-text', 'String Literal');
+              addLexeme('"', 'blue-text', 'String Delimeter');
+              break;
+          }
+        }
+        else{
+          // add variable
+          addLexeme(identifier, 'white-text', 'Variable Identifier');
+        }
       }
       else if(/\s*GIMMEH\s+/.test(lines[i])){
-        addLexeme('GIMMEH', 'green-text', 'Input identifier');
+        addLexeme('GIMMEH', 'green-text', 'Input Identifier');
 
+        // remove whitespaces
         identifier = lines[i].substr(7).trim();
 
         // add identifier
-        addLexeme(identifier, 'white-text', 'Variable identifier');
+        addLexeme(identifier, 'white-text', 'Variable Identifier');
       }
     }
 
@@ -122,7 +210,7 @@ angular.module('app', []).controller('AppController', function($scope){
         identifier = $scope.lexemes[++i].lexeme.text;
         $scope.lexemeIndex++;
 
-        addSymbol(identifier, 'NOOB', 'red-text', 'NOOB', 'red-text');
+        addSymbol(identifier, 'NOOB', 'yellow-text', 'NOOB', 'yellow-text');
       }
       else if($scope.lexemes[i].lexeme.text == "GIMMEH"){
         identifier = $scope.lexemes[++i].lexeme.text;
@@ -140,6 +228,52 @@ angular.module('app', []).controller('AppController', function($scope){
         // break run
         return;
       }
+      else if($scope.lexemes[i].lexeme.text == 'ITZ'){
+        // get identifier and value
+        identifier = $scope.lexemes[(i - 1)].lexeme.text;
+        var value = $scope.lexemes[++i].lexeme.text;
+        $scope.lexemeIndex++;
+
+        var typeText;
+        var valueColor;
+
+        // identify typeText
+        if(regex.NOOB.test(value)){
+          // NOOB
+          typeText = 'NOOB';
+          valueColor = 'yellow-text';
+        }
+        else if(regex.TROOF.test(value)){
+          // TROOF
+          typeText = 'TROOF';
+          valueColor = 'red-text';
+        }
+        else if(regex.NUMBR.test(value)){
+          // NUMBR
+          typeText = 'NUMBR';
+          valueColor = 'white-text';
+        }
+        else if(regex.NUMBAR.test(value)){
+          // NUMBAR
+          typeText = 'NUMBAR';
+          valueColor = 'white-text';
+        }
+        else if(value == '"'){
+          // YARN
+          typeText = 'YARN';
+          valueColor = 'blue-text';
+
+          value = '"' + $scope.lexemes[++i].lexeme.text + '"';
+          i++;
+          $scope.lexemeIndex += 2;
+        }
+        else{
+          // variable
+        }
+
+        // edit symbol
+        editSymbol(identifier, typeText, 'yellow-text', value, valueColor);
+      }
     }
 
     // end of program
@@ -149,40 +283,13 @@ angular.module('app', []).controller('AppController', function($scope){
   $scope.input = function(){
     var identifier = $scope.identifier;
     var value = $('#input').val();
-    var typeText;
-    var typeColor;
-    var valueText;
-    var valueColor;
+    var typeText = 'YARN';
+    var typeColor = 'yellow-text';
+    var valueText = '"' + value + '"';
+    var valueColor = 'blue-text';
 
     // add to console
-    $scope.console.push({text: '> '+value});
-
-    if(!value){
-      typeText = 'NOOB';
-      typeColor = 'red-text';
-      valueText = 'NOOB';
-      valueColor = 'red-text';
-    }
-    else if(/^\d+$/.test(value)){
-      typeText = 'NUMBR';
-      typeColor = 'yellow-text';
-      valueText = value;
-      valueColor = 'white-text';
-    }
-    else if(/^\d+\.\d+$/.test(value)){
-      // NUMBARs
-      typeText = 'NUMBAR';
-      typeColor = 'yellow-text';
-      valueText = value;
-      valueColor = 'white-text';
-    }
-    else{
-      typeText = 'YARN';
-      typeColor = 'yellow-text';
-      valueText = '"' + value + '"';
-      valueColor = 'blue-text';
-      // YARN
-    }
+    printToConsole(value);
 
     editSymbol(identifier, typeText, typeColor, valueText, valueColor);
 
@@ -246,5 +353,11 @@ angular.module('app', []).controller('AppController', function($scope){
     $scope.symbolTable[index].type.color = typeColor;
     $scope.symbolTable[index].value.text = valueText;
     $scope.symbolTable[index].value.color = valueColor;
+  }
+
+  function printToConsole(text){
+    $scope.console.push({
+      text: '> ' + text
+    });
   }
 });
